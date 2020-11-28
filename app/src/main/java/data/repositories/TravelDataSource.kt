1831d8435753c9b991a0f8cf6798714e1091beef
arilazar <57072150+arilazar@users.noreply.github.com>
2020-11-29 00:06:45 +0200

    date picker

--- app/src/main/java/data/repositories/TravelDataSource.kt
+++ app/src/main/java/data/repositories/TravelDataSource.kt
@@ -30,8 +30,4 @@ class TravelDataSource {
 //            });
 
     }
-
-
-
-
 }
--- app/src/main/java/ui/AddTravelActivity.kt
+++ app/src/main/java/ui/AddTravelActivity.kt
@@ -1,16 +1,24 @@
 package ui
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
+import android.text.InputType
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.traveldeal.R
-import com.google.firebase.database.FirebaseDatabase
 import data.entities.Travel
+import java.util.*
+
+
+//import com.emedinaa.kotlinmvvm.di.Injection
 
 
 class AddTravelActivity : AppCompatActivity() {
 
+    lateinit var viewModel: TravelViewModel
     lateinit var saveButton: Button
     lateinit var etClientName: EditText
     lateinit var etPhone: EditText
@@ -22,35 +30,87 @@ class AddTravelActivity : AppCompatActivity() {
     lateinit var etDestinationAddress: EditText
     lateinit var spinnerRequestStatus: Spinner
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_travel)
+
+        //viewModel = ViewModelProvider(this)[TravelViewModel::class.java]
+        viewModel = ViewModelProvider(this).get(TravelViewModel::class.java)
+
+//        val isSuccess: LiveData<Travel> = viewModel.getItems()
+//        isSuccess.observe(this,
+//            Observer<Travel> {
+//                Toast.makeText(applicationContext, "הפרטים נשמרו בהצלחה!", Toast.LENGTH_SHORT)
+//                    .show()
+//
+//            })
+
+
+//            viewModel = ViewModelProvider(
+//                this,
+//                Injection.provideViewModelFactory()
+//            ).get(TravelViewModel::class.java)
 
         saveButton = findViewById(R.id.buttonSave)
         etClientName = findViewById(R.id.editTextClientName)
         etPhone = findViewById(R.id.editTextPhone)
         etEmailAddress = findViewById(R.id.editTextEmailAddress)
-        etDepartureDate = findViewById(R.id.editTextDepartureDate)
+        //etDepartureDate = findViewById(R.id.editTextDepartureDate)
         etReturnDate = findViewById(R.id.editTextReturnDate)
         etPassengersNumber = findViewById(R.id.editTextPassengersNumber)
         etDepartureAddress = findViewById(R.id.editTextTextDepartureAddress)
         etDestinationAddress = findViewById(R.id.editTextTextDestinationAddress)
         spinnerRequestStatus = findViewById(R.id.spinnerRequestStatus)
 
-        val statusEnum = arrayOf("SEND", "RECEIVED", "RUN", "CLOSED")
+        var picker: DatePickerDialog
+        etDepartureDate = findViewById<View>(R.id.editTextDepartureDate
+        ) as EditText
+        etDepartureDate.setInputType(InputType.TYPE_NULL)
+        etDepartureDate.setOnClickListener(View.OnClickListener {
+            val cldr: Calendar = Calendar.getInstance()
+            val day: Int = cldr.get(Calendar.DAY_OF_MONTH)
+            val month: Int = cldr.get(Calendar.MONTH)
+            val year: Int = cldr.get(Calendar.YEAR)
+            // date picker dialog
+            picker = DatePickerDialog(
+                this,
+                OnDateSetListener { view, year, monthOfYear, dayOfMonth -> etDepartureDate.setText(dayOfMonth.toString() + "/" + (monthOfYear + 1) + "/" + year) },
+                year,
+                month,
+                day
+            )
+            picker.show()
+        })
 
-        val adapter: ArrayAdapter<String> = ArrayAdapter(
-            this, android.R.layout.simple_spinner_item, statusEnum
-        )
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinnerRequestStatus.setAdapter(adapter)
-        //spinner.setOnItemSelectedListener(this)
+        ArrayAdapter.createFromResource(
+
+            this, R.array.status_array, android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerRequestStatus.adapter = adapter
+        }
+
+
+
+
+//        saveButton.addTextChangedListener(textWatcher)
+        //etClientName.addTextChangedListener(textWatcher)
+//        etPhone.addTextChangedListener(textWatcher)
+//        etEmailAddress.addTextChangedListener(textWatcher)
+//        etDepartureDate.addTextChangedListener(textWatcher)
+//        etReturnDate.addTextChangedListener(textWatcher)
+//        etPassengersNumber.addTextChangedListener(textWatcher)
+//        etDepartureAddress.addTextChangedListener(textWatcher)
+//        etDestinationAddress.addTextChangedListener(textWatcher)
+//        spinnerRequestStatus.toString().addTextChangedListener(textWatcher)
     }
 
+
     fun saveButton(view: View) {
-        val rootNode = FirebaseDatabase.getInstance()
-        val reference = rootNode.getReference("travels")
 
         val clientName = etClientName.text.toString()
         val clientPhone = etPhone.text.toString()
@@ -63,6 +123,7 @@ class AddTravelActivity : AppCompatActivity() {
         val requestStatus = spinnerRequestStatus.toString()
 
         val travel = Travel(
+
             clientName,
             clientPhone,
             clientEmailAddress,
@@ -74,28 +135,33 @@ class AddTravelActivity : AppCompatActivity() {
             requestStatus
         )
 
-        reference.push().setValue(travel)
+
+        viewModel.insertItem(travel)
 
         val text = "הפרטים נשמרו בהצלחה!"
         val toast = Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT)
         toast.show()
     }
 
-//    private val textWatcher = object : TextWatcher {
-//        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//            TODO("Not yet implemented")
+
+//    val textWatcher = object : TextWatcher {
+//        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//            val tex = etClientName.text.toString().trim()
+//            saveButton.isEnabled = !(
+//                    tex.isEmpty())
+////                    ||etPhone.text.toString().trim().isEmpty() ||
+////                    etEmailAddress.text.toString().trim().isEmpty() ||
+////                    etDepartureDate.text.toString().trim().isEmpty() ||
+////                    etReturnDate.text.toString().trim().isEmpty() ||
+////                    etPassengersNumber.text.toString().trim().isEmpty() ||
+////                    etDepartureAddress.text.toString().trim().isEmpty() ||
+////                    etDestinationAddress.text.toString().trim().isEmpty() ||
+////                    spinnerRequestStatus.toString().trim().isEmpty())
 //        }
 //
-//        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//            buttonSave1.isEnabled = !(editTextClientName1.text.isEmpty() ||
-//                    editTextPhone1.text.isEmpty() ||
-//                    _editTextEmailAddress.text.isEmpty() ||
-//                    _editTextDepartureDate.text.isEmpty() ||
-//                    _editTextReturnDate.text.isEmpty() ||
-//                    _editTextPassengersNumber.text.isEmpty() ||
-//                    _editTextTextDepartureAddress.text.isEmpty() ||
-//                    _editTextTextDestinationAddress.text.isEmpty() ||
-//                    _editTextTextRequestStatus.text.isEmpty())
+//
+//        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//            TODO("Not yet implemented")
 //        }
 //
 //        override fun afterTextChanged(s: Editable?) {
@@ -103,5 +169,6 @@ class AddTravelActivity : AppCompatActivity() {
 //        }
 //    }
 }
+
 
 

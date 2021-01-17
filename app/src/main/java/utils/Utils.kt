1150eaa2f439ec28060e3f1eae10fe7c67006ef6
arilazar <57072150+arilazar@users.noreply.github.com>
2021-01-17 21:09:28 +0200

    decode company in spinner

---
+++ app/src/main/java/utils/Utils.kt
@@ -0,0 +1,16 @@
+package utils
+
+
+class Utils {
+    companion object {
+
+        fun decodeKey(key: String?): String {
+            return key!!.replace("\\u002e", ".").replace("\\u0024", "\$").replace("\\\\", "\\")
+        }
+
+        fun encodeKey(key: String?): String {
+            return key!!.replace("\\", "\\\\").replace("\$", "\\u0024").replace(".", "\\u002e")
+        }
+
+    }
+}
